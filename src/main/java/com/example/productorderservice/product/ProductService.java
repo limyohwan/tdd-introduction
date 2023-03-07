e0ea56bb1ed0fd2e6427a5891a484ec08f710c9a
Yohwan Lim <dyghks7102@naver.com>
2023-03-07 21:40:14 +0900

    api 호출 테스트

--- src/main/java/com/example/productorderservice/product/ProductService.java
+++ src/main/java/com/example/productorderservice/product/ProductService.java
@@ -1,8 +1,15 @@
 package com.example.productorderservice.product;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Component
+@RestController
+@RequestMapping("/products")
 class ProductService {
     private final ProductPort productPort;
 
@@ -10,9 +17,12 @@ class ProductService {
         this.productPort = productPort;
     }
 
-    public void addProduct(AddProductRequest request) {
+    @PostMapping
+    public ResponseEntity addProduct(@RequestBody AddProductRequest request) {
         final Product product = new Product(request.getName(), request.getPrice(), request.getDiscountPolicy());
 
         productPort.save(product);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
