dfdece8af043f51a3e97f2a57dbf18a275dcd52a
Yohwan Lim <dyghks7102@naver.com>
2023-03-08 21:26:05 +0900

    스프링 부트 테스트로 전환

--- src/test/java/com/example/productorderservice/product/ProductServiceTest.java
+++ src/test/java/com/example/productorderservice/product/ProductServiceTest.java
@@ -1,51 +1,38 @@
 package com.example.productorderservice.product;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest
 class ProductServiceTest {
+
+    @Autowired
     private ProductService productService;
+
+    @Autowired
     private ProductPort productPort;
-
-    @BeforeEach
-    void setUp(){
-        productPort = Mockito.mock(ProductPort.class);
-
-        productService = new ProductService(productPort);
-    }
+    
     @Test
     void 상품수정(){
+        productService.addProduct(ProductSteps.상품등록요청_생성());
         Long productId = 1L;
         UpdateProductRequest request = new UpdateProductRequest("상품 수정", 2000, DiscountPolicy.NONE);
-        Product product = new Product("상품명", 1000, DiscountPolicy.NONE);
-
-        Mockito.when(productPort.getProduct(productId)).thenReturn(product);
 
         productService.updateProduct(productId, request);
 
-        assertThat(product.getName()).isEqualTo("상품 수정");
-        assertThat(product.getPrice()).isEqualTo(2000);
+        ResponseEntity<GetProductResponse> response = productService.getProduct(productId);
+        GetProductResponse productResponse = response.getBody();
+        assertThat(productResponse.getName()).isEqualTo("상품 수정");
+        assertThat(productResponse.getPrice()).isEqualTo(2000);
 
     }
 
-//    private static class StubProductPort implements ProductPort {
-//        public Product getProduct_will_return;
-//
-//        @Override
-//        public void save(Product product) {
-//
-//        }
-//
-//        @Override
-//        public Product getProduct(Long productId) {
-//            return getProduct_will_return;
-//        }
-//    }
 }
