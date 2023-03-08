4524b879232096fef760cf701226a4e3d22f56fb
Yohwan Lim <dyghks7102@naver.com>
2023-03-08 23:43:24 +0900

    api 테스트로 변경

--- src/test/java/com/example/productorderservice/payment/PaymentService.java
+++ src/test/java/com/example/productorderservice/payment/PaymentService.java
@@ -1,9 +1,17 @@
 package com.example.productorderservice.payment;
 
 import com.example.productorderservice.order.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Component
+@RestController
+@RequestMapping("/payments")
 class PaymentService {
     private final PaymentPort paymentPort;
 
@@ -11,12 +19,16 @@ class PaymentService {
         this.paymentPort = paymentPort;
     }
 
-    public void payment(PaymentRequest request) {
+    @PostMapping
+    @Transactional
+    public ResponseEntity<Void> payment(@RequestBody PaymentRequest request) {
         Order order = paymentPort.getOrder(request.getOrderId());
 
         Payment payment = new Payment(order, request.getCardNumber());
 
         paymentPort.pay(payment.getPrice(), payment.getCardNumber());
         paymentPort.save(payment);
+
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 }
--- src/test/java/com/example/productorderservice/payment/PaymentSteps.java
+++ src/test/java/com/example/productorderservice/payment/PaymentSteps.java
@@ -1,11 +1,25 @@
 package com.example.productorderservice.payment;
 
 import com.example.productorderservice.payment.PaymentRequest;
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.springframework.http.MediaType;
 
 public class PaymentSteps {
     public static PaymentRequest 주문결제요청_생성() {
         Long orderId = 1L;
         String cardNumber = "1234-1234-1234-1234";
         return new PaymentRequest(orderId, cardNumber);
+    }
+
+    static ExtractableResponse<Response> 주문결제요청(PaymentRequest request) {
+        return RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(request)
+                .when()
+                .post("/payments")
+                .then().log().all()
+                .extract();
     }
 }
